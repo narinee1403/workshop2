44b2690457e204d548e669a6c61401de5507997c
narinee1403 <narinee.sr@ku.th>
2021-01-10 16:01:56 +0700

    final exercise

--- variable/exercise.py
+++ variable/exercise.py
@@ -12,22 +12,22 @@ x = 5
 y  = 10
 print( x + y)
 
-4.Create a variable called z, assign x + y to it, and display the result.
+#4.Create a variable called z, assign x + y to it, and display the result.
 
 x = 5
 y = 10
 z = x + y
 print(z)
 
-5.Remove the illegal characters in the variable name:
+#.Remove the illegal characters in the variable name:
 
 my_first_name = "John"
 
-6.Insert the correct syntax to assign the same value to all three variables in one code line.
+#.Insert the correct syntax to assign the same value to all three variables in one code line.
 
 x=y=z = "Orange"
 
-7.Insert the correct keyword to make the variable x belong to the global scope.
+#.Insert the correct keyword to make the variable x belong to the global scope.
 
 def myfunc():
   golbal x
